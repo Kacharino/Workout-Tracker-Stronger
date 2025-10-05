c41f4419235f50eb7fc67bb4877ed09763692e7f
Kacha Narimanidze <narimanidze2000@gmail.com>
2025-10-05 17:59:11 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## 🏋️‍♂️ Projektbeschreibung
+
+**Eine einfache und schnelle App für Kraftsportler im Fitnessstudio**, um ihre Trainingseinheiten zu protokollieren und ihren Fortschritt bei den Hauptübungen zu verfolgen.
---
+++ src/test/java/at/kacharino/workouttrackerstronger/WorkoutTrackerStrongerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kacharino.workouttrackerstronger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerStrongerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
